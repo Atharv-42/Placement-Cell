f2035f4ad69011204c06244106e93d65cbf597e3
Atharv-42 <dhokaneatharv@gmail.com>
2026-06-05 17:09:57 +0530

    Fix: apply CORS and JSON body parser before auth routes; restore DB connect

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const connectDB = require("./config/db");
 
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
@@ -16,8 +15,12 @@ const companyRoutes = require("./routes/companyRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
 
+const authRoutes = require("./routes/authRoutes");
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.use("/api/students", studentRoutes);
 app.use("/api/companies", companyRoutes);
